93f5ac6051025c7001baa9b8fb153b15102ef064
shahnawaz sayyed <shahnawazsayyed456@gmail.com>
2022-02-07 15:00:15 +0530

    Added

---
+++ .history/HackerRank/forloop_20220207144650.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <limits.h>
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+int main()
+{
+
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    for (int i = a; i <= b; i++)
+    {
+        if(a == 1){
+        printf("%s" , "one");
+    }
+    else if(a == 2){
+        printf("%s" , "two");
+    }
+    else if(a == 3){
+        printf("%s" , "three");
+    }
+    else if(a == 4){
+        printf("%s" , "four");
+    }
+    else if(a == 5){
+        printf("%s" , "five");
+    }
+    else if(a == 6){
+        printf("%s" , "six");
+    }
+    else if(a == 7){
+        printf("%s" , "seven");
+    }
+    else if(a == 8){
+        printf("%s" , "eight");
+    }
+    else if(a == 9){
+        printf("%s" , "nine");
+    }
+    else if(a > 9 && a % 2 == 0){
+        printf("%s" , "even");
+    }
+    else if(a > 9 && a % 2 == 1){
+        printf("%s" , "odd");
+    }
+    }
+    
+}
---
+++ .history/HackerRank/forloop_20220207145200.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <limits.h>
+#define vi vector<int>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+int main()
+{
+
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    for (int i = a; i <= b; i++)
+    {
+        int main() 
+{
+    int a, b;
+    int n=0;
+    scanf("%d\n%d", &a, &b);
+  	for(n=a;n<=b;n++)
+    {
+    if(n==1)
+    {
+        printf("one\n");
+    }
+    if(n==2)
+    {
+        printf("two\n");
+    }
+    if(n==3)
+    {
+        printf("three\n");
+    }
+    if(n==4)
+    {
+        printf("four\n");
+    }
+    if(n==5)
+    {
+        printf("five\n");
+    }
+    if(n==6)
+    {
+        printf("six\n");
+    }
+    if(n==7)
+    {
+        printf("seven\n");
+    }
+    if(n==8)
+    {
+        printf("eight\n");
+    }
+    if(n==9)
+    {
+        printf("nine\n");
+    }
+     if(n>9)
+    {
+       if(n%2==0)
+       {
+           printf("even\n");
+       }
+       else {
+       printf("odd\n");
+       }    
+    }
+
+      }
+    return 0;
+}
+    }
+}
